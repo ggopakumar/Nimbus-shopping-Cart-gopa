0960ee6cb3a09e1bef77f29416146f4c1e716b99
ggopakumar <51371711+ggopakumar@users.noreply.github.com>
2019-08-17 19:51:49 +0530

    Update EtailerUiApplication.java

--- src/main/java/com/etailer/etailerui/EtailerUiApplication.java
+++ src/main/java/com/etailer/etailerui/EtailerUiApplication.java
@@ -1,7 +1,6 @@
 package com.etailer.etailerui;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
